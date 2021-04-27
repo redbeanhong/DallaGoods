8db069f863cb22cbce2317bc46b6b59cd6ed4cc7
redbean <67220689+redbeanhong@users.noreply.github.com>
2021-04-27 17:40:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  created() {
+    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
+    this.$http.get(api).then((res) => {
+      console.log(res);
+    });
+  },
+};
+</script>
+
+<style lang="scss">
+  @import '~bootstrap/scss/bootstrap'
+</style>
